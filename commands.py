60d0ad3b445879bc12f481c88c90b65597bd9812
Guillem Castro <guillem.castro@hotmail.com>
2017-11-16 14:16:15 +0100

    Updated /start command

--- commands.py
+++ commands.py
@@ -2,11 +2,9 @@ from BotConfig import BotConfig
 import FIB
 
 def start(bot, update):
-    reply = "Hi! I'm RacoAssistant, a bot that will blablabla. " \
-            "Please follow this link to authorize the bot: \n" + FIB.get_auth_url(BotConfig.client_id, BotConfig.complete_host,
-                                                                                  update.effective_user.id)
-    print(reply)
-    update.message.reply_text(reply)
+    reply = "Hi! I'm RacoAssistant, a bot that will blablabla." \
+    " Please follow <a href=\"" + FIB.get_auth_url(BotConfig.client_id, BotConfig.complete_host, update.effective_user.id) + "\">this link</a> to authorize the bot"
+    update.message.reply_text(reply, parse_mode='HTML')
 
 def stop(bot, update):
     update.message.reply_text("<a href=\"www.hackupc.com\">hackupc</a>", parse_mode="HTML")
